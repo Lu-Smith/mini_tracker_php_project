eda81a72af58dfc5b4d046f739e7cfde96d163ab
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-10 14:46:09 +0000

    Updated App.php

--- app/App.php
+++ app/App.php
@@ -1,3 +1,13 @@
 <?php
  
  declare(strict_types = 1);
+
+ function getTransactionFiles(): array
+ {
+    $files = [];
+
+    foreach(scandir(FILES_PATH) AS $file) 
+    {
+        var_dump($file);
+    }
+ }
