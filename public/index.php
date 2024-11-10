632d27bad4d567365b038179590a66be89107592
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-10 15:00:33 +0000

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,9 @@ declare(strict_types = 1);
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
-define('FILE_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+require APP_PATH . 'App.php';
+
+getTransactionFiles();
